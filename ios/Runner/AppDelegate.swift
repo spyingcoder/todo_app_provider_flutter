f07a478bb48cf9b8993c1493c8213b8a0079597c
Johnny <johnny.ayush@rubicotech.in>
2022-09-12 17:39:34 +0530

    Fix: Firebase configurtion on iOS.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import FirebaseCore
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,9 +7,7 @@ import FirebaseCore
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
